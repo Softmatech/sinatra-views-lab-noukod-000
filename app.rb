d9909834965e53b9711d5fd45f60e38aa1243937
Andy Feidje Joseph <ingandy75@gmail.com>
2019-06-27 21:09:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 class App < Sinatra::Base
 
-get '/' do
-	erb :index
-end
-
-	get '/hello' do
-		erb :hello
+	get '/' do
+		erb :index
 	end
-
-get '/goodbye' do
-	erb :goodbye
-end
-
-get '/date' do
-	erb :date
-end
+	
+	get '/goodbye' do
+	  erb :goodbye
+	end
+	
+	get '/hello' do 
+	  erb :hello
+	end
+	
+	get '/date' do 
+	  erb :date
+	end
 
 end
